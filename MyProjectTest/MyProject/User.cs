336d835ba092eee4dd91282bdb0426ce94ea2248
Cyntia Aparecida Pereira Dos Santos <c.pereira.dos.santos@avanade.com>
2021-10-21 09:41:30 -0300

    xunit added

---
+++ MyProjectTest/MyProject/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyProject
+{
+    public class User
+    {
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        private int MaiorIdade = 18;
+
+        public object MaiorDeIdade()
+        {
+            if(Age<MaiorIdade)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ MyProjectTest/MyProjectTest/Example1.cs
@@ -0,0 +1,36 @@
+﻿using MyProject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MyProjectTest
+{
+    public class Example1
+    {
+        [Fact]
+        public void UsuarioEMaiorDeIdade()
+        {
+            //Arrange
+            var user = new User("José", 50);
+            var expected = true;
+            //Act
+            var result = user.MaiorDeIdade();
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void UsuarioEMenorDeIdade()
+        {
+            //Arrange
+            var user = new User("João", 17);
+            var expected = false;
+            //Act
+            var result = user.MaiorDeIdade();
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
